7858ac0de3dd2eb2fe4e10516188e7475e2a30d7
Jager1020 <juanlg123ab@outlook.es>
2023-02-23 18:40:03 -0500

    FinPlantilla

--- js/menu.js
+++ js/menu.js
@@ -1,5 +1,6 @@
 const hamburguerIcon = document.querySelector('.nav__hamburguer');
-const navOverlay = document.querySelector('.nav__overlay')
+const navOverlay = document.querySelector('.nav__overlay');
+let currentDropdown = navOverlay;
 
 hamburguerIcon.addEventListener('click', ()=>{
     hamburguerIcon.classList.toggle('nav__hamburguer--open');
@@ -23,14 +24,39 @@ navOverlay.addEventListener('click', (e)=>{
 
             subMenu.style.height = `${height}px`
 
+        }else {
+        
+            if( !isActive(subMenu, 'nav__inner--show')){
+                closeDropdown(currentDropdown);
+            }
+            subMenu.classList.toggle('nav__inner--show');
+    
+            currentDropdown = subMenu
+    
         }
 
-    }else {
-        
     }
 });
 
 function isActive(element, string){
     return element.classList.value.includes(string);
 }
+
+function closeDropdown(currentDropdown){
+    if( isActive(currentDropdown, 'nav__inner--show')){
+        currentDropdown.classList.remove('nav__inner--show')
+    }
+}
+
+window.addEventListener('resize', ()=>{
+    closeDropdown(currentDropdown);
+
+    if(window.innerWidth > 768){
+        const navInner = document.querySelectorAll('.nav__inner');
+
+        navInner.forEach(navInner =>{
+            navInner.style.height = '';
+        });
+    }
+})
 
